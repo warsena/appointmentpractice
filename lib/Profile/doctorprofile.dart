21a4ea42a5707f2751e79783cced0f893b46cc53
warsena <dnaawarsena@gmail.com>
2025-01-05 22:24:24 +0800

    remove unuse import file

--- lib/Profile/doctorprofile.dart
+++ lib/Profile/doctorprofile.dart
@@ -2,7 +2,6 @@ import 'package:appointmentpractice/Profile/doctoreditprofile.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:appointmentpractice/Profile/usereditprofile.dart';
 
 class DoctorProfile extends StatefulWidget {
   const DoctorProfile({super.key});
